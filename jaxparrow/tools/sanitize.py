da4e250e08201d71ea5d5897a52abf87c1ee9e20
Vadim Bertrand <vadim.bertrand@univ-grenoble-alpes.fr>
2024-04-29 09:50:53 +0200

    fix how the mask is initialized when unspecified

--- jaxparrow/tools/sanitize.py
+++ jaxparrow/tools/sanitize.py
@@ -32,7 +32,7 @@ def sanitize_data(
     return arr
 
 
-def init_mask(
+def init_land_mask(
         field: Float[Array, "lat lon"],
         mask: Float[Array, "lat lon"] = None
 ) -> Float[Array, "lat lon"]:
@@ -55,7 +55,7 @@ def init_mask(
         Initialized (if needed) mask
     """
     if mask is None:
-        mask = jnp.isfinite(field)
+        mask = ~jnp.isfinite(field)
     return mask
 
 
--- tests/test_velocities.py
+++ tests/test_velocities.py
@@ -3,7 +3,7 @@ import optax
 from jaxparrow.cyclogeostrophy import _iterative, _variational, LR_VAR
 from jaxparrow.geostrophy import _geostrophy
 from jaxparrow.tools.operators import interpolation
-from jaxparrow.tools.sanitize import init_mask
+from jaxparrow.tools.sanitize import init_land_mask
 
 import gaussian_eddy as ge
 
@@ -15,48 +15,45 @@ class TestVelocities:
         u_geos_est, v_geos_est = _geostrophy(self.ssh, self.dXY, self.dXY, self.coriolis_factor)
         u_geos_est_t = interpolation(u_geos_est, axis=1, padding="left")
         v_geos_est_t = interpolation(v_geos_est, axis=0, padding="left")
-        geos_rmse = self.compute_rmse(self.u_geos, self.v_geos, u_geos_est_t, v_geos_est_t)  # around 0.0008
-        assert geos_rmse < .001
+        geos_rmse = self.compute_rmse(self.u_geos, self.v_geos, u_geos_est_t, v_geos_est_t)  # around 0.0003
+        assert geos_rmse < .0005
 
     def test_cyclogeostrophy_penven(self):
-        mask = init_mask(self.u_geos)
-        u_geos_u = interpolation(self.u_geos, axis=1, padding="right")
-        v_geos_v = interpolation(self.v_geos, axis=0, padding="right")
-        u_cyclo_est, v_cyclo_est, _ = _iterative(u_geos_u, v_geos_v,
+        mask = init_land_mask(self.ssh)
+        u_geos_est, v_geos_est = _geostrophy(self.ssh, self.dXY, self.dXY, self.coriolis_factor)
+        u_cyclo_est, v_cyclo_est, _ = _iterative(u_geos_est, v_geos_est,
                                                  self.dXY, self.dXY, self.dXY, self.dXY,
                                                  self.coriolis_factor, self.coriolis_factor, mask,
                                                  20, 0.01, False, 3, False)
         u_cyclo_est_t = interpolation(u_cyclo_est, axis=1, padding="left")
         v_cyclo_est_t = interpolation(v_cyclo_est, axis=0, padding="left")
-        cyclo_rmse = self.compute_rmse(self.u_cyclo, self.v_cyclo, u_cyclo_est_t, v_cyclo_est_t)  # around .0035
-        assert cyclo_rmse < .004
+        cyclo_rmse = self.compute_rmse(self.u_cyclo, self.v_cyclo, u_cyclo_est_t, v_cyclo_est_t)  # around .002
+        assert cyclo_rmse < .003
 
     def test_cyclogeostrophy_ioannou(self):
-        mask = init_mask(self.u_geos)
-        u_geos_u = interpolation(self.u_geos, axis=1, padding="right")
-        v_geos_v = interpolation(self.v_geos, axis=0, padding="right")
-        u_cyclo_est, v_cyclo_est, _ = _iterative(u_geos_u, v_geos_v,
+        mask = init_land_mask(self.ssh)
+        u_geos_est, v_geos_est = _geostrophy(self.ssh, self.dXY, self.dXY, self.coriolis_factor)
+        u_cyclo_est, v_cyclo_est, _ = _iterative(u_geos_est, v_geos_est,
                                                  self.dXY, self.dXY, self.dXY, self.dXY,
                                                  self.coriolis_factor, self.coriolis_factor, mask,
                                                  20, 0.01, True, 3, False)
         u_cyclo_est_t = interpolation(u_cyclo_est, axis=1, padding="left")
         v_cyclo_est_t = interpolation(v_cyclo_est, axis=0, padding="left")
-        cyclo_rmse = self.compute_rmse(self.u_cyclo, self.v_cyclo, u_cyclo_est_t, v_cyclo_est_t)  # around .0035
-        assert cyclo_rmse < .004
+        cyclo_rmse = self.compute_rmse(self.u_cyclo, self.v_cyclo, u_cyclo_est_t, v_cyclo_est_t)  # around .002
+        assert cyclo_rmse < .003
 
     def test_cyclogeostrophy_variational(self):
-        mask = init_mask(self.u_geos)
-        u_geos_u = interpolation(self.u_geos, axis=1, padding="right")
-        v_geos_v = interpolation(self.v_geos, axis=0, padding="right")
+        mask = init_land_mask(self.ssh)
+        u_geos_est, v_geos_est = _geostrophy(self.ssh, self.dXY, self.dXY, self.coriolis_factor)
         optim = optax.sgd(learning_rate=LR_VAR)
-        u_cyclo_est, v_cyclo_est, _ = _variational(u_geos_u, v_geos_v,
+        u_cyclo_est, v_cyclo_est, _ = _variational(u_geos_est, v_geos_est,
                                                    self.dXY, self.dXY, self.dXY, self.dXY,
                                                    self.coriolis_factor, self.coriolis_factor, mask,
-                                                   20, optim, False)
+                                                   1000, optim, False)
         u_cyclo_est_t = interpolation(u_cyclo_est, axis=1, padding="left")
         v_cyclo_est_t = interpolation(v_cyclo_est, axis=0, padding="left")
-        cyclo_rmse = self.compute_rmse(self.u_cyclo, self.v_cyclo, u_cyclo_est_t, v_cyclo_est_t)  # around .0035
-        assert cyclo_rmse < .004
+        cyclo_rmse = self.compute_rmse(self.u_cyclo, self.v_cyclo, u_cyclo_est_t, v_cyclo_est_t)  # around .002
+        assert cyclo_rmse < .003
 
     @staticmethod
     def compute_rmse(u, v, u_est, v_est) -> float:
